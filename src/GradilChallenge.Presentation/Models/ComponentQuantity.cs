0f3948f67ab6b1852aedcb4c6e03d6aa28bc4296
PabloQuadros <pablo.aquadros@gmail.com>
2026-07-05 03:18:40 -0300

    feat(presentation): add MVVM base components

---
+++ src/GradilChallenge.Presentation/Models/ComponentQuantity.cs
@@ -0,0 +1,7 @@
+﻿namespace GradilChallenge.Presentation.Models;
+
+public class ComponentQuantity
+{
+    public string Name { get; set; } = "";
+    public int Quantity { get; set; }
+}
---
+++ src/GradilChallenge.Presentation/ViewModels/AsyncRelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Input;
+
+namespace GradilChallenge.Presentation.ViewModels;
+
+public sealed class AsyncRelayCommand : ICommand
+{
+    private readonly Func<Task> _execute;
+    private readonly Func<bool>? _canExecute;
+    private readonly Action<Exception> _onException;
+    private bool _isExecuting;
+
+    public AsyncRelayCommand(Func<Task> execute, Action<Exception> onException, Func<bool>? canExecute = null)
+    {
+        _execute = execute;
+        _onException = onException;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter) => !_isExecuting && (_canExecute?.Invoke() ?? true);
+
+    public async void Execute(object? parameter)
+    {
+        _isExecuting = true;
+        RaiseCanExecuteChanged();
+        try
+        {
+            await _execute();
+        }
+        catch (Exception ex)
+        {
+            _onException(ex);
+        }
+        finally
+        {
+            _isExecuting = false;
+            RaiseCanExecuteChanged();
+        }
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public void RaiseCanExecuteChanged() => CommandManager.InvalidateRequerySuggested();
+}
